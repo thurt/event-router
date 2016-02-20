5bc6d591ddf185d79cd93650c8bc5a848f0e0751
Taylor Hurt <taylor.a.hurt@gmail.com>
2016-02-20 01:15:30 +0000

    update getSubscribers to return deep copy

--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 'use strict'
+const copyObjectGraph = require('copy-object-graph')
+const myName = 'EventRouter'
+
 function EventRouter() {
-  const subscribers = {}
+  const subscribers = Object.create(null)
 
   // public interface
   return {
     getSubscribers() {
-      return Object.assign({}, subscribers)
+      return copyObjectGraph(subscribers)
     },
 
     add(type, key, callback) {
-      console.info('EventRouter:', 'add', type, key)
+      console.info(myName, 'add', type, key)
       _add(type, key, callback, subscribers)
     },
 
     remove(type, key, callback) {
-      console.info('EventRouter:', 'remove', type, key)
+      console.info(myName, 'remove', type, key)
       _remove(type, key, callback, subscribers)
     },
 
     send(type, key, data) {
-      console.info('EventRouter:', 'send', type, key)
+      console.info(myName, 'send', type, key)
       _send(type, key, data, subscribers)
+    },
+
+    purge(type) {
+      console.info(myName, 'purge', type)
+      _purge(type, subscribers)
     }
   }
 }
 
-module.exports = EventRouter
+module.exports = EventRouter()
 /////////////////////////////////////////////////////
 /*
   t = type
@@ -34,11 +42,15 @@ module.exports = EventRouter
   o = object
   cb = callback
 */
+function _purge(t, o) {
+  if (!o[t]) return console.warn(myName, 'event type', t, 'cannot be purged because it does not exist')
+  delete o[t]
+}
 function _send(t, k, d, o) {
   var ot = o[t]
-  if (!ot) return console.warn(`EventRouter: event type ${t} ${k} was just fired, but there are no registered callbacks.` )
+  if (!ot) return console.warn(myName, 'event type', t, k, 'was just fired but there are no registered callbacks')
   var otk = ot[k]
-  if (!otk) return console.warn(`EventRouter: event type ${t} ${k} was just fired, but there are no registered callbacks.`)
+  if (!otk) return console.warn(myName, 'event type', t, k, 'was just fired but there are no registered callbacks')
 
   for (let cb of otk) cb(d)
 }
@@ -53,10 +65,10 @@ function _add(t, k, cb, o) {
 }
 function _remove(t, k, cb, o) {
   var ot = o[t]
-  if (!ot) return console.warn(`EventRouter: cannot find type ${t}`)
+  if (!ot) return console.warn(myName, 'cannot find type', t)
   var otk = ot[k]
-  if (!otk) return console.warn(`EventRouter: cannot find key ${k} in type ${t}`)
-  if (!otk.includes(cb)) return console.warn(`EventRouter: cannot find callback ${cb} in key ${k} in type ${t}`)
+  if (!otk) return console.warn(myName, 'cannot find key', k, 'in type', t)
+  if (!otk.includes(cb)) return console.warn(myName, 'cannot find callback', cb, 'in key', k, 'in type', t)
 
   otk.splice(otk.indexOf(cb), 1)
 
